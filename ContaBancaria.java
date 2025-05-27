5f86d8846ea03f51a882c2f351b97f598b1fd77b
Thiago Pereira Sousa <JT0020362@mateus.dc>
2025-05-27 08:19:10 -0300

    bank system java

---
+++ ContaBancaria.java
@@ -0,0 +1,73 @@
+
+import java.util.Scanner;
+
+public class ContaBancaria{
+
+    private String name;
+    private int agencia;
+    private String contaCorrente;
+    private double saldo;
+
+    public ContaBancaria(String name,int agencia,String contaCorrente, double saldo){
+       this.name = name;
+       this.agencia= agencia;
+       this.contaCorrente = contaCorrente;
+       this.saldo = saldo;
+    }
+
+     public String setName(String name){
+        return this.name = name;
+     }
+
+      public String getName(){
+        return name;
+     }
+     public double getSaldo(){
+        return saldo;
+     }
+
+     public double setSaldo(double saldo){
+        return this.saldo = saldo;
+     }
+
+     public int getAgencia() {
+        return agencia;
+     }
+
+     public int setAgencia(int agencia){
+       return this.agencia = agencia;
+     }
+
+     public String getContaCorrente(){
+        return contaCorrente;
+     }
+     public String setContaCorrente(String contaCorrente){
+          return this.contaCorrente = contaCorrente;
+     }
+
+     public void Depositar(){
+         
+         Scanner scanner = new Scanner(System.in);
+
+
+        int valorDeposito = scanner.nextInt();
+
+        if(valorDeposito > 0){
+            this.saldo += valorDeposito;
+
+            System.out.println("o saldo atual da conta e : " + saldo );
+        }else{
+            System.out.println("deposito invalido,por favor,escolha um numero superior a 0 para depositar");
+        }
+     }
+
+     public void Sacar(){
+        Scanner scanner = new Scanner(System.in);
+
+        int valorParaSaque = scanner.nextInt();
+
+        if(this.saldo >= valorParaSaque){
+            this.saldo-= valorParaSaque;
+        }
+     }
+}
