9d004c28e37a978978fa5a88d260436c1068375c
Thiago Pereira Sousa <JT0020362@MATEUS.DC>
2025-05-27 08:45:30 -0300

    created the main file in java to get the methods of anothers files

---
+++ Principal.java
@@ -0,0 +1,14 @@
+
+
+public class Principal {
+  public static void main(String[] args) {
+      
+      ContaBancaria contaBancaria = new ContaBancaria();
+
+      contaBancaria.getSaldo();
+      contaBancaria.Depositar();
+      contaBancaria.getName();
+      contaBancaria.Sacar();
+
+  }
+}
